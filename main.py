3c1981668f526c4446ab4570ddebbfef99a70c67
Crowfoood <sandorclegane34@yahoo.gr>
2017-06-15 23:17:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,44 @@
-#import multiprocessing, time, pika, json, traceback, logging, sys, os, itertools, urllib, urllib2, cStringIO, mysql.connector, shutil, hashlib, socket, urllib2, re
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-#from PIL import Image
-from os import listdir
-from os.path import isfile, join
-#from bs4 import BeautifulSoup
-from pprint import pprint
 import webdriver_class
-
-
-wdc = webdriver_class
-wdc.appointment_search()import webdriver_class
 from tkinter import *
 import threading
+import traceback
 
 root = Tk()
-root.geometry("600x300")
+root.geometry("440x180")
 frame = Frame(root)
 root.resizable(width=False, height=False)
 wdc = webdriver_class
 
 def logIn():
     try:
-        t = threading.Thread(target=wdc.appointment_search, args=(email.get("1.0",'end-1c'), pw.get("1.0",'end-1c')))
+        t = threading.Thread(target=wdc.appointment_search, args=(email_entry.get(), pw_entry.get()))
         t.daemon = True
         t.start()
     except:
-        print
-        "Error: unable to start thread"
+        traceback.print_exc()
+        #print("Error: unable to start thread")
 
 
+email_label = Label( text='Email: ')
+pw_label = Label(text='Password: ')
+email_entry = Entry()
+pw_entry = Entry()
+button1 = Button(text="Log In", height=2, width=15, command=logIn)
 
-button1 = Button(frame, text="Log In", height=2, width=15, command=logIn)
-email = Text(frame, height=2, width=30)
-pw = Text(frame, height=2, width=30)
+email_label.grid(row=0, column=2)
+pw_label.grid(row=1, column=2)
+email_entry.grid(row=0, column=3)
+pw_entry.grid(row=1, column=3)
+button1.grid(row=4, column=3, columnspan=3)
 
 frame.bind("<Key>", logIn)
-frame.grid(column=0, row=7, padx=20, pady=40)
 
-frame.pack()
-email.pack()
-pw.pack()
-button1.pack()
+
+
 
 
 
 root.mainloop()
-
 
 
 
